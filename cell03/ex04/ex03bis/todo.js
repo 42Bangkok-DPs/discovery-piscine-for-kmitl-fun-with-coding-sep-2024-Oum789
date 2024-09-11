b3d14f699644a0cbacacf078f9a901f4d7752449
KRITTAMET KITUMNUAYPONG <110441420+Oum789@users.noreply.github.com>
2024-09-12 01:22:01 +0700

    Update todo.js

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -3,15 +3,19 @@ $(document).ready(function() {
         const tdArray = $('#ft_list .TD').map(function() {
             return $(this).text();
         }).get();
-        document.cookie = `tds=${JSON.stringify(tdArray)}`;
-        console.log(document.cookie);
+        str_array = JSON.stringify(tdArray);
+        en_str_array = encodeURIComponent(str_array);
+        document.cookie = `tds=${en_str_array}`;
+        // console.log(document.cookie);
     }
 
     function loadTDs() {
-        const cookies = document.cookie;
-        console.log(cookies);
-        if (cookies) {
-            const tdArray = JSON.parse(cookies.split('=')[1]);
+        const cookies = document.cookie.split("=")[1];
+        // console.log(cookies);
+        de_cookie = decodeURIComponent(cookies);
+        // console.log(de_cookie);
+        if (de_cookie) {
+            const tdArray = JSON.parse(de_cookie);
             tdArray.forEach(tdText => {
                 const $TD = $('<div>').text(tdText).addClass('TD');
                 $('#ft_list').append($TD);
